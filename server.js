9d147861c9f34fdaaf5b2c26a3fc7affab117690
Aditya19 <adellikar@gmail.com>
2026-01-17 19:39:43 +0530

    fixed cors origins

--- server.js
+++ server.js
@@ -27,7 +27,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5173",
-  process.env.CLIENT_URL,
+  "https://dreamdecor-frontend.vercel.app",
+ // process.env.CLIENT_URL,
 ];
 
 app.use(
@@ -43,7 +44,7 @@ app.use(
       }
     },
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
